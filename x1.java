7b620eddd44bcd8eda755c0cf0576ef3ce810d2c
Khaled <110719720+kshible24259@users.noreply.github.com>
2022-08-06 14:35:02 +0200

    Loops in java
    try make practices

---
+++ x1.java
@@ -0,0 +1,11 @@
+/*package whatever //do not write package name here */
+// Java program to write a code in for loop from 1 to 10
+
+class GFG {
+	public static void main(String[] args)
+	{
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
