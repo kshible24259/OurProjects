13da393b33e47fb5ed0b76b4a5e05d8406059904
Khaled <kshible24259@yahoo.com>
2022-08-06 12:44:37 +0200

    Adding first Java Programm to the repo

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+// This is a simple Java program.
+// FileName : "HelloWorld.java".
+
+public class helloworld
+{
+// Your program begins with a call to main().
+// Prints "Hello, World" to the terminal window.
+   public static  void  main(String args[])
+{
+for(int i=0 ;i<12;i++){
+System.out.println("test1");
+//hqsjhqksj
+}
+
+}
+}
